2cc08daf634fa26287d2f58c2d002e98c45f1df9
Daniil Burnashov <danil-burnashov@mail.ru>
2023-03-28 19:37:25 +0300

    правка

--- More_Test/More_Test/Services/NetworkService.swift
+++ More_Test/More_Test/Services/NetworkService.swift
@@ -13,34 +13,31 @@ import PromiseKit
 struct NetworkService: NetworkServiceProtocol {
     // MARK: - Private properties
     
-    private let usersProvider = MoyaProvider<GitHub<Any>>()
-    private let networkQueue = DispatchQueue(label: "networkQueue")
+    private let usersProvider = MoyaProvider<GitHub<Any>>(callbackQueue: DispatchQueue.main)
     
     // MARK: - Public methods
     
     func fetchUsers(method: GitHub<Any>, request: RequestType) -> Promise<[User]> {
         return Promise { seal in
-            networkQueue.async {
-                usersProvider.request(method) { result in
-                    switch result {
-                    case let .success(response):
-                        do {
-                            guard request == .users else {
-                                guard
-                                    let user = try? JSONDecoder().decode(User.self, from: response.data)
-                                else { return }
-                                seal.fulfill([user])
-                                return
-                            }
+            usersProvider.request(method) { result in
+                switch result {
+                case let .success(response):
+                    do {
+                        guard request == .users else {
                             guard
-                                let users = try? JSONDecoder().decode([User].self, from: response.data)
+                                let user = try? JSONDecoder().decode(User.self, from: response.data)
                             else { return }
-                            seal.fulfill(users)
+                            seal.fulfill([user])
                             return
                         }
-                    case let .failure(error):
-                        seal.reject(error)
+                        guard
+                            let users = try? JSONDecoder().decode([User].self, from: response.data)
+                        else { return }
+                        seal.fulfill(users)
+                        return
                     }
+                case let .failure(error):
+                    seal.reject(error)
                 }
             }
         }
